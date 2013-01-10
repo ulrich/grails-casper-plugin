51dc92a2593ef081d0d2b501c52c814fde897857
Ulrich VACHON <u.vachon@gmail.com>
2013-01-10 22:04:19 +0100

    Removed Casper and Phantom utilities, use local install instead of

--- casper/grails-app/utils/casper/tests/suites/casper/elementattribute.js
+++
@@ -1,8 +0,0 @@
-casper.start('tests/site/elementattribute.html', function() {
-    this.test.comment('Casper.getElementAttribute()');
-    this.test.assertEquals(this.getElementAttribute('.testo','data-stuff'), 'beautiful string', 'Casper.getElementAttribute() works as intended');
-});
-
-casper.run(function() {
-    this.test.done();
-});
